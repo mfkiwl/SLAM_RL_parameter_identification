324083b1966569b428f3909b196d82e92a98655d
rodrigo-munguia <rodrigo.munguia@gmail.com>
2024-02-01 13:36:03 -0600

    Init

---
+++ gym-examples/gym_examples/__init__.py
@@ -0,0 +1,9 @@
+#from gym.envs.registration import register
+
+from gymnasium.envs.registration import register
+
+register(
+    id="gym_examples/GridWorld-v0",
+    entry_point="gym_examples.envs:GridWorldEnv",
+    
+)
---
+++ gym-examples/gym_examples/envs/__init__.py
@@ -0,0 +1,4 @@
+from gym_examples.envs.grid_world import GridWorldEnv
+
+
+
